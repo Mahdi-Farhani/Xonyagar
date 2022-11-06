ef836b18d6069acbd0a43452202a4dea702d2523
Mahdi Farhani <mm.farhani@gmail.com>
2022-11-06 11:15:01 +0330

    Add project files.

---
+++ Partikan.Xonyagar/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Partikan.Xonyagar.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
